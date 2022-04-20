62317a5dbbc0d8caf0b74434551222a378c65c26
Hosni Marco <hosni.marco@gmail.com>
2022-04-20 13:49:59 +0200

    ex 5 6 and 8

--- Ejercicio 5 JS IF/index.js
+++ Ejercicio 5 JS IF/index.js
@@ -1,4 +1,4 @@
-function askNumber() {
+function ej5() {
 	let askNumber = prompt("Introduzca un numero");
 	const getElement = document.getElementById("result");
 	if (!isNaN(askNumber)) {
@@ -11,5 +11,25 @@ function askNumber() {
 		}
 	} else {
 		getElement.innerHTML = `El valor ${askNumber} no es un valor numerico`;
+	}
+}
+function ej6() {
+	let askNumber = prompt("Introduzca un numero");
+	const getElement = document.getElementById("result");
+	if (!isNaN(askNumber) && askNumber < 0) {
+		getElement.innerHTML = `El numero ${askNumber} es menor a 0`;
+	} else if (!isNaN(askNumber) && askNumber > 99999999) {
+		getElement.innerHTML = `El numero ${askNumber} es mayor a 99999999`;
+	}
+}
+function ej8() {
+	let askNumber = prompt("Introduzca un numero");
+	const getElement = document.getElementById("result");
+	if (!isNaN(askNumber)) {
+		if (askNumber % 2 == true) {
+			getElement.innerHTML = `El numero ${askNumber} es impar`;
+		} else {
+			getElement.innerHTML = `El numero ${askNumber} es par`;
+		}
 	}
 }
